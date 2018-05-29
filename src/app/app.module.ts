6bc359a37fb6e44b38f0da1b4838a598c9d7c98a
harshadsalape <harshad.salape@wwindia.com>
2018-05-29 21:06:15 +0530

    added the components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AsideComponent } from './aside.component';
+import { FooterComponent } from './footer.component';
+import { HeaderComponent } from './header.component';
+import { SectionComponent } from './section.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AsideComponent,
+    FooterComponent,
+    HeaderComponent,
+    SectionComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent, AsideComponent, FooterComponent, HeaderComponent, SectionComponent]
 })
 export class AppModule { }
